ad380eebe5b950761241ff645007d6ec1e2a72bd
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2020-12-08 15:25:48 -0800

    Interview Scheduler Project with React. Created DayList and DayListItem to display the days, and InterviewList and InterviewListItem to display interviewers.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classnames from "classnames";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+   return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const dayClass = classnames("day-list", {
+    "day-list__item": props.name,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = function(spots) {
+
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+    if (spots === 1 ) {
+      return `${spots} spot remaining`;
+    }
+    return `${spots} spots remaining`;
+  };
+  
+  return (
+    <li className={dayClass} selected={props.selected} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      <h3>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
